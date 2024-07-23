219eca33aae7576270f3b97df8d60e598c47a3e2
js <jspaul@caltech.edu>
2024-07-24 00:56:34 +0200

    Update 12gen.py
    draft for 12gen.py w/ multiprocessing
    
    analysis.py has mistakes for nupack functions. that file is deprecated for the moment, use methods defined in 12gen.py

--- src/seqwalk/12gen.py
+++ src/seqwalk/12gen.py
@@ -1,6 +1,7 @@
 import numpy as np
 import multiprocessing.pool.ThreadPool
 from tqdm import tqdm
+import tqdm
 '''
 wget “http://46.101.39.40/nupack_public/nupack-4.0.1.11.zip”
 unzip -q ‘nupack-4.0.1.11.zip’
@@ -8,6 +9,7 @@ pip install -U nupack -f nupack-4.0.1.11/package
 '''
 import nupack as nu
 from seqwalk import design
+ncores = 32
 
 RT = nu.Model(material="dna", celsius=30)
 
@@ -90,19 +92,62 @@ def nupack_matrix(library, model=RT, conc=1e-6, RCfree=False):
 
     np_probs = np.zeros((len(library), len(library)))
     print("PART 1/2")
-	
    for i in tqdm(range(len(library))):
         for j in range(i+1, len(library)):
-            p =  1 - np_crosstalk(library[i], library[j], model, conc)[0]/conc
+            p =  1 - np_crosstalk(library[i], library[j], model, conc, RCfree)[0]/conc
             np_probs[i, j] += p
             np_probs[j, i] += p
+		
     print("\n PART 2/2")
     for i in tqdm(range(len(library))):
-        np_probs[i, i] += np_crosstalk(library[i], library[j], model, conc)[0]/conc
+        np_probs[i, i] += np_crosstalk(library[i], library[i], model, conc, RCfree)[0]/conc
+    return np_probs
+
+def ij_np_mat_mp_helper(library, model, conc, RCfree, i, j, np_probs):
+    assert(np_probs[i,j] = 0 and np_probs[j,i] == 0) 
+    # safety check in case i misunderstood original code, avoid race conditions
+    if i != j:
+        p =  1 - np_crosstalk(library[i], library[j], model, conc, RCfree)[0]/conc
+    else:
+        p = np_crosstalk(library[i], library[i], model, conc, RCfree)[0]/conc
+    np_probs[i, j] += p
+    np_probs[j, i] += p
+
+def nupack_matrix_mp(library, model=RT, conc=1e-6, RCfree=False):
+    """
+    matrix where element i, j is binding prob between seq i and seq j
+
+    Args:
+        library: list of seqs
+        model: nupack conditions (default RT)
+        conc: molar concentrations of strands (default 1e-6)
+        RCfree: Bool, True if library is to be RC free
+
+    Returns:
+        NxN numpy array : np_probs
+            binding probability heatmap
+    """
+
+    np_probs = np.zeros((len(library), len(library)))
+    print("BEGINNING")
+    # create the thread pool
+    n = len(library)
+    with ThreadPool(ncores) as pool:
+        with tqdm.tqdm(total= n*(n+1)/2) as pbar:
+            for i in range(n):
+                for j in range(i, n):
+                    # issue task
+                    _ = pool.apply_async(ij_np_mat_mp_helper, args=(library, model, conc, RCfree, i, j, np_probs), callback=pbar.update(1))	
+        # close the pool
+        pool.close()
+        # wait for tasks to complete
+        pool.join()
+
     return np_probs
 
 
+# run my design job
 library = design.max_size(12, 6, alphabet="ACGT")
-nu_mat = nupack_matrix(library)
+nu_mat = nupack_matrix_mp(library, conc=1e-8, RCfree=True)
 np.save("nu_mat", nu_mat)
 print(nu_mat)
